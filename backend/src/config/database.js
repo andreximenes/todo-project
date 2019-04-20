09120e21e12537057369337fbb26f2f5306e5f56
André Luiz Ximenes <andreluziximenes@gmail.com>
2019-04-20 13:57:58 -0300

    .

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/todo')
+module.exports = mongoose.connect(
+    'mongodb+srv://ominiuser:ominiuser@cluster0-cj3ll.mongodb.net/todoappc?retryWrites=true',
+    { userNewUrlParser: true },
+    err => {
+        if (err) throw err;
+        console.log(`Successfully connected to database.`);
+    }
+)
